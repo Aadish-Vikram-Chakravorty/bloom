3757dd28730bfa3374b588db5da537fcb82a9ce6
Aadish Vikram Chakravorty <aadishchakravorty@gmail.com>
2025-03-13 04:18:52 +0530

    Commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,19 @@
 import React from 'react'
-
+import Link from 'next/link'
+import { MobileMenu } from './MobileMenu'
 export const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+    return (
+        <div className='h-24 flex items-center justify-between'>
+            {/* LEFT */}
+            <div className=''>
+                <Link href='/' className='text-xl font-bold text-blue-800 '>Bloom</Link>
+            </div>
+            {/* CENTER */}
+            <div className='hidden'></div>
+            {/* RIGHT */}
+            <div className=''>
+                <MobileMenu />
+            </div>
+        </div>
+    )
 }
