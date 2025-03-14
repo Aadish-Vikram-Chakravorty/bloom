965e7e3e0e41cd89fd910273979f275c9d6c444f
Aadish Vikram Chakravorty <aadishchakravorty@gmail.com>
2025-03-15 03:19:50 +0530

    Commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
 import { MobileMenu } from './MobileMenu'
+import Image from 'next/image'
 export const Navbar = () => {
     return (
         <div className='h-24 flex items-center justify-between'>
@@ -9,10 +10,26 @@ export const Navbar = () => {
                 <Link href='/' className='text-xl font-bold text-blue-800 '>Bloom</Link>
             </div>
             {/* CENTER */}
-            <div className='hidden'></div>
-            {/* RIGHT */}
-            <div className=''>
-                <MobileMenu />
+            <div className='hidden md:flex w-[50%]'>
+                {/*LINKS*/}
+                <div className='flex gap-6 text-gray-600'>
+                    <Link href="/" className="flex items-center gap-2" >
+                        <Image src='/home.png' alt="Homepage" width={16} height={16} className='w-4 h-4' />
+                        <span>Homepage</span>
+                    </Link>
+                    <Link href="/" className="flex items-center gap-2" >
+                        <Image src='/friends.png' alt="Friends" width={16} height={16} className='w-4 h-4' />
+                        <span>Friends</span>
+                    </Link>
+                    <Link href="/" className="flex items-center gap-2" >
+                        <Image src='/stories.png' alt="Stories" width={16} height={16} className='w-4 h-4' />
+                        <span>Stories</span>
+                    </Link>
+                </div>
+                {/* RIGHT */}
+                <div className='w-[30%] flex items-center justify-end gap-4 xl:gap-8 '>
+                    <MobileMenu />
+                </div>
             </div>
         </div>
     )
